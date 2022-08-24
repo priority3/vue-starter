e2781033feffe20bc693acceb2572e40c5783da2
priority <1832052104@qq.com>
2022-08-24 22:09:26 +0800

    fixed: unocss intellisense

--- config/plugin/index.ts
+++ config/plugin/index.ts
@@ -2,7 +2,8 @@ import type { PluginOption } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Layouts from 'vite-plugin-vue-layouts'
-import { configUnocss } from './unocss'
+// import { configUnocss } from './unocss'
+import Unocss from 'unocss/vite'
 import { configVuecomponents } from './vue-auto-com'
 import { configHtmlPLugin } from './html'
 import type { ViteEnv } from '#/config'
@@ -10,10 +11,11 @@ export function configVitePlugin(viteEnv: ViteEnv) {
   const vitePlugins: (PluginOption | PluginOption[])[] = [
     vue(),
     vueJsx(),
+    Unocss(),
     Layouts(),
   ]
   // unocss
-  vitePlugins.push(configUnocss())
+  // vitePlugins.push(configUnocss())
   // vue auto components
   vitePlugins.push(configVuecomponents())
   // html
